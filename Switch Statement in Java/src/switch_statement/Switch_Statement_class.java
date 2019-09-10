89a294a3e26639dd7bd504c1f86097ebe48a6e33
deshanr <deshanr@QAPPS02.QAPPSTECH.IN>
2019-09-10 18:05:09 +0530

    commit

---
+++ Switch Statement in Java/src/switch_statement/Switch_Statement_class.java
@@ -0,0 +1,36 @@
+package switch_statement;
+
+
+
+public class Switch_Statement_class 
+{
+	public static void main(String[] args) 
+	{
+		
+		// initialize the switch statement in a block
+		
+		String name="joseph";
+		
+		switch(name)
+		{
+		case "bevin": 
+		 
+			System.out.println("print the bevin");
+			break;
+		
+		case "deshan":
+			System.out.println("print the deshan");
+			break;
+			
+		case "joseph":
+			System.out.println("print the joseph");
+			break;
+			
+		default:
+			
+			System.out.println("invalid name form a list");
+		}
+			
+	}
+
+}
---
+++ Switch Statement in Java/src/switch_statement/Swith_Statement_class2.java
@@ -0,0 +1,30 @@
+package switch_statement;
+
+public class Swith_Statement_class2
+{
+
+	static char c='A';
+	
+	public static void main(String[] args)
+	{
+		switch(c)
+		{
+		case 'A':
+			System.out.println("print the A value");
+			break;
+			
+		
+		case 'B':
+			System.out.println("print the B value");
+			
+		case 'C':
+			System.out.println("print the C value");
+		
+		default:
+			
+			System.out.println("grade invalid vaule");
+			
+		}
+	}
+	
+}
